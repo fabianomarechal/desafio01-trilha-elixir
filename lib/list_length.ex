aed5b9b3dbfd1766d78e61cc576c5a7fd72085a6
Fabiano Leite <fabiano.marechal@gmail.com>
2022-02-02 21:51:46 -0300

    Desafio concluido

--- lib/list_length.ex
+++ lib/list_length.ex
@@ -1,5 +1,14 @@
 defmodule ListLength do
-  def call() do
-    #TO DO
+  def call(list) do
+    length_calc(list, 0)
+  end
+
+  defp length_calc([], num) do
+    num
+  end
+
+  defp length_calc([_head | tail], num) do
+    num = num + 1
+    length_calc(tail, num)
   end
 end
